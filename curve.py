a99117af0efc6bf53aecbc451b2a8f81d75cbf0e
davincif <LDaVinciF@gmail.com>
2017-05-23 10:56:44 -0300

    bcurve toggle_show() working

--- curve.py
+++ curve.py
@@ -17,7 +17,7 @@ class Curve:
 	psize = 3 #the radius of the point size, in pixels
 	thickness = 1 #default width of the line
 	__shown = True #if the curve is being shown or not
-	color = 'black' #default line color
+	color = '#000000' #default line color
 
 	#OVERWRITE METHODS
 	def __init__(self):
@@ -32,7 +32,8 @@ class Curve:
 		elif self.__bezier:
 			string += " bezier "
 		else:
-			string += "grau " + str(self.__degree) + " "
+			string += "grau " + str(self.__degree)
+			string += " delc "
 
 		string += str(self.printable())
 		return string
@@ -55,12 +56,12 @@ class Curve:
 			raise Exception("This curve is already a master curve, can't be bezier as well")
 		self.__bezier = True
 		self.__degree = -1
-		self.color = "blue"
+		self.color = "#0000ff"
 
-	def isShown(self):
+	def is_shown(self):
 		return self.__shown
 
-	def toggleShow(self):
+	def toggle_show(self):
 		self.__shown = not self.__shown
 
 	def show(self, option):
@@ -68,6 +69,30 @@ class Curve:
 			self.__shown = option
 		else:
 			raise Exception("toogle the show stat you need to pass a bool argument")
+
+	def toggle_derivated_show(self):
+		for elem in self.__delc:
+			elem.toggle_show()
+
+	def change_color(self, rpg):
+		self.color = '#%02x%02x%02x' % rpg
+		# r, g , b = rgb
+		# hex(r) + hex(g)[2:] + hex(b)[2:]
+
+	def decay_color(self, percentual, rgblimit):
+		h = "0x" + self.color[1:len(self.color)]
+		h1, h2, h3 = h[0:4], '0x' + h[4:6], '0x' + h[6:8]
+		r, g , b = int(h1, 16), int(h2, 16), int(h3, 16)
+		r = r + (255*percentual)
+		g = g + (255*percentual)
+		b = b + (255*percentual)
+		if r > rgblimit[0]:
+			r = rgblimit[0]
+		if g > rgblimit[1]:
+			g = rgblimit[1]
+		if b > rgblimit[2]:
+			b = rgblimit[2]
+		self.change_color((r,g,b))
 
 
 	#COMMON METHODS
@@ -99,18 +124,38 @@ class Curve:
 		return lp
 
 	def draw(self, canva):
+		if self.__master:
+			canva.delete("master")
+		elif self.__bezier:
+			canva.delete("bezier")
+		else:
+			canva.delete("derivative"+str(self.__degree))
+
 		if self.__shown:
 			n = 0
 			while n < self.__lopS:
-				canva.create_oval(self.__lop[n].x - self.psize, self.__lop[n].y - self.psize, self.__lop[n].x + self.psize, self.__lop[n].y + self.psize, fill=self.color)
+				item = canva.create_oval(self.__lop[n].x - self.psize, self.__lop[n].y - self.psize, self.__lop[n].x + self.psize, self.__lop[n].y + self.psize, fill=self.color)
+				if self.__master:
+					canva.itemconfig(item, tags="master")
+				elif self.__bezier:
+					canva.itemconfig(item, tags="bezier")
+				else:
+					canva.itemconfig(item, tags="derivative"+str(self.__degree))
 				if n != self.__lopS-1:
-					canva.create_line(self.__lop[n].x, self.__lop[n].y, self.__lop[n+1].x, self.__lop[n+1].y, width=self.thickness, fill=self.color)
+					item = canva.create_line(self.__lop[n].x, self.__lop[n].y, self.__lop[n+1].x, self.__lop[n+1].y, width=self.thickness, fill=self.color)
+					if self.__master:
+						canva.itemconfig(item, tags="master")
+					elif self.__bezier:
+						canva.itemconfig(item, tags="bezier")
+					else:
+						canva.itemconfig(item, tags="derivative"+str(self.__degree))
 				n += 1
 
 		if self.__delc is not None:
 			itr = 0 #iterator
 			while itr < self.__degree:
-				self.__delc[itr].draw(canva)
+				if self.__delc[itr].__shown:
+					self.__delc[itr].draw(canva)
 				itr += 1
 
 		if self.__bcurve is not None:
@@ -127,6 +172,7 @@ class Curve:
 			print("The curve must have at least degree 1, but has only " + str(self.__degree))
 		elif self.__delc is not None and len(self.__delc) > 0:
 			print("This curve already was derivated")
+			self.toggle_derivated_show()
 		else:
 			prevc = self #previous curve
 			newc = self #current curve
@@ -135,6 +181,7 @@ class Curve:
 			while itr1 < self.__degree:
 				self.__delc += [Curve()]
 				self.__delc[itr1].__lop = []
+				self.__delc[itr1].color = self.color
 				if itr1 == 0:
 					prevc = self
 					newc = self.__delc[itr1]
@@ -146,18 +193,20 @@ class Curve:
 					#derivate
 					newc.__add_point(0.5*prevc.__lop[itr2] + 0.5*prevc.__lop[itr2+1])
 					itr2 += 1
+				newc.decay_color(0.3, (230, 230, 230))
 				itr1 += 1
-			newc.color = "Blue"
+			newc.change_color((0, 0, 255))
 
 	def calcBezier(self):
 		if self.__bcurve is not None:
 			print("Bezier curve is already calculated")
+			self.__bcurve.toggle_show()
 		elif self.__degree > 1:
 			#ok calculate it!
 			self.__bcurve = Curve()
+			self.__bcurve.makeBezier()
 			bcaux = self.__bcurve #Bezier curve auxiliar
 			bcaux.__lop = []
-			bcaux.makeBezier()
 			intervals = 2
 			t = 1/intervals
 
